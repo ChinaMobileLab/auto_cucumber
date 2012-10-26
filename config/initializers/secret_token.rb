0c4a06e2fe57687d66ec97e19816bdceb58f615a
lishoubo <litttlan@gmail.com>
2012-10-26 11:13:02 +0800

    init project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MobileTestWeb::Application.config.secret_token = 'b4223f639d8caeefe5db551fcedca73ad50466221906d9ce59d0ae84e19580297c37d0f9ef1355eabaaa9a6a1d14da3c2d13c02adf5f7cc1776b61ef72ba7a26'
