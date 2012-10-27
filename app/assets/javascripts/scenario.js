d98307fca95f7935e2a402bc3896f7a9c8d34ba8
lishoubo <litttlan@gmail.com>
2012-10-27 18:13:00 +0800

    add from selection

---
+++ app/assets/javascripts/scenario.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+  $("#histoy_givens").change(function(){
+  	var selected = $(this).children("option:selected")[0].text
+  	var last_field = $("#histoy_givens").parent().children(".fields").last()
+  	$(last_field).children("textarea").val(selected)
+  });
+     
+});
